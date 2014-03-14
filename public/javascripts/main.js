ab3c875534c33bbceac5998c21607701663eab78
Eugene Wen <newenegue@gmail.com>
2014-03-14 16:45:04 -0700

    add and remove articles to bucket with double click.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,21 @@
+// ---------------------------------------------------------------------------
+// ------------------------------------------
+// Create articles for development
+// ------------------------------------------
+var artArray = [];
+for(var i = 0; i < 50; i++){
+  artArray.push({
+    'title': 'title' + i,
+    'id': i,
+    'text': 'Of course I peed my pants, everyone my age pees their pants. I ate some Triscuit crackers in the car, you should have had some. Knibb High football rules! This guy can stay in my room, I can tell you that much. I thought I was your snack-pack? When I graduated first grade, all my dad did was tell me to get a job.'
+  });
+}
+
+// ---------------------------------------------------------------------------
+
+// Global bucketArray
+var bucketArray = [];
+
 // ------------------------------------------
 // jQuery
 // ------------------------------------------
@@ -9,28 +27,57 @@ $(document).ready(function() {
     console.log("Your popularity is " + (++popCount));
   });
 
+  // Double click article to add to bucket
+  $(document.body).on('dblclick', '.article' ,function(){
+    // THIS IS TOO MESSY!!!
+    var article_id = $($(this).first()).children()[0].id;
+
+    // Check if article exists in bucket, if not, add it
+    if(bucketArray.indexOf(article_id) == -1){
+
+      bucketArray.push(article_id);
+      // Adjust DOM, make article not draggable and gray out
+      $("#" + article_id).parent().addClass("article_in_bucket");
+      $("#" + article_id).attr({"draggable": false });
+      refreshBucket();
+    }
+  });
+
+  // Double click article to remove from bucket
+  $(document.body).on('dblclick', '.bucket_item' ,function(){
+    // MESSY!!!!!
+    var article_id = $($(this).first()).attr("data-id");
+
+    // Find article and remove it from bucketArray
+    var index = bucketArray.indexOf(article_id);
+    bucketArray.splice(index,1);
+    // Adjust DOM, make article draggable
+    $("#" + article_id).parent().removeClass("article_in_bucket");
+    $("#" + article_id).attr({"draggable": true });
+    refreshBucket();
+  });
+
 });
 
 // ------------------------------------------
 // Updating bucket list
 // ------------------------------------------
-// Global bucketArray
-var bucketArray = [];
-
 // Refresh the bucket without refreshing the entire page
 function refreshBucket() {
   // Reinitialize bucket content
   var bucketContent = '';
 
-  // loop through new bucket array and add content
+  // Loop through new bucket array and add content
   for(var i = 0; i < bucketArray.length; i++) {
     // console.log($("#" + bucketArray[i])[0]);
+    bucketContent += '<div class="bucket_item" data-id="' + bucketArray[i] + '">';
     bucketContent += '<div>' + $("#" + bucketArray[i] + " .title b")[0].innerHTML + '</div>';
     bucketContent += '<button ng-click="makePopular()">Read</button>';
+    bucketContent += '</div>';
     // bucketContent += '<div>' + bucketArray[i] + '</div><br>';
   }
 
-  // inject html with new bucket content
+  // Inject html with new bucket content
   $(".bucket").html(bucketContent);
 }
 
@@ -48,24 +95,17 @@ function allowDrop(ev) {
 
 function drop(ev) {
   ev.preventDefault();
-  var id = ev.dataTransfer.getData("article_id");
-  bucketArray.push(id);
-  $("#" + id).parent().addClass("article_in_bucket");
-  $("#" + id).attr({"draggable": false });
-  // Increase popularity of ID HERE ****************
-  refreshBucket();
-}
+  var article_id = ev.dataTransfer.getData("article_id");
+  // Add to bucket
+  bucketArray.push(article_id);
+  // Adjust DOM, make article not draggable and gray out
+  $("#" + article_id).parent().addClass("article_in_bucket");
+  $("#" + article_id).attr({"draggable": false });
 
-// ------------------------------------------
-// Create articles for development
-// ------------------------------------------
-var artArray = [];
-for(var i = 0; i < 50; i++){
-  artArray.push({
-    'title': 'title' + i,
-    'id': i,
-    'text': 'Of course I peed my pants, everyone my age pees their pants. I ate some Triscuit crackers in the car, you should have had some. Knibb High football rules! This guy can stay in my room, I can tell you that much. I thought I was your snack-pack? When I graduated first grade, all my dad did was tell me to get a job.'
-  });
+  // Increase popularity of ID HERE ****************
+  // Notify database to increase popularity of article with article_id
+  
+  refreshBucket();
 }
 
 // ------------------------------------------
@@ -75,6 +115,7 @@ var peekApp = angular.module('PeekApp', []);
 
 peekApp.controller('PeekCtrl', function($scope) {
   $scope.articles = artArray;
+  // $scope.popularity;
   var popularity = 0;
 
   $scope.makePopular = function() {
