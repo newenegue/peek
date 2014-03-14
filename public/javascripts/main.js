0b5e2c7019e909307084be3e8744b287f388a51b
Eugene Wen <newenegue@gmail.com>
2014-03-14 11:50:04 -0700

    added hg-repeat for article divs.  added hg-click.  need to work on popularity.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,6 +11,16 @@ $(document).ready(function() {
 
 });
 
+// ------------------------------------------
+// Create articles for development
+// ------------------------------------------
+var artArray = [];
+for(var i = 0; i < 50; i++){
+  artArray.push({
+    'title': 'title' + i,
+    'text': 'Of course I peed my pants, everyone my age pees their pants. I ate some Triscuit crackers in the car, you should have had some. Knibb High football rules! This guy can stay in my room, I can tell you that much. I thought I was your snack-pack? When I graduated first grade, all my dad did was tell me to get a job.'
+  });
+}
 
 // ------------------------------------------
 // Angular
@@ -18,7 +28,13 @@ $(document).ready(function() {
 var peekApp = angular.module('PeekApp', []);
 
 peekApp.controller('PeekCtrl', function($scope) {
-  console.log('Inside PeekCtrl');
-  $scope.articles = [];
+  // console.log('Inside PeekCtrl');
+  $scope.articles = artArray;
+  var popularity = 0;
+
+  $scope.makePopular = function() {
+    popularity++;
+    console.log(popularity);
+  };
 });
 
