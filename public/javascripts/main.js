674228c62b7432dd33b6fa1ba81ecd534a5288de
ElenaHoshizaki <elena.hoshizaki@gmail.com>
2014-03-18 12:22:17 -0700

    add shadow to animated bucket. need to fix shadow animation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -106,6 +106,18 @@ $(document).ready(function() {
       }
   });
 
+  // ------------------------------------------
+  // Animated bucket: shadow
+  // ------------------------------------------   
+  $("#shadowStage").load('images/shadow_bucket.svg',function(response){
+
+      $(this).addClass("svgLoaded");
+       
+      if(!response){
+        console.log("Error loading SVG!");
+      }
+  });
+
 });
 
 // ------------------------------------------
