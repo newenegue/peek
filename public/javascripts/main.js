959669f5adfbd97628df492242e09d4464db3361
Eugene Wen <newenegue@gmail.com>
2014-03-19 16:49:19 -0700

    stash

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,8 +10,7 @@ $(document).ready(function() {
   // Close Peek reader
   $(document.body).on('click', '.close', closeArticle);
 
-  // Double click article to remove from bucket
-  $(document.body).on('dblclick', '.bucket_item', removeSelectedItem);
+  // Click remove button
   $(document.body).on('click', '.remove_bucket_item', removeSelectedItem);
 
   // Animated bucket
@@ -28,6 +27,7 @@ function readArticle() {
 
   var article_id = $($(this).parent()).attr('data-id');
   var paragraph = $.parseJSON($("#" + article_id).attr('data-paragraph'));
+  var link = $("#" + article_id).attr('data-article-link');
 
   read(paragraph);
 
