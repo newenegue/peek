33e87830d8db5f2710cd12082732f7674139de0a
Kendrick Parks <bordersmbk02@aol.com>
2014-03-21 11:03:11 -0700

    bucket found by cookie

--- public/javascripts/bucketHandler.js
+++ public/javascripts/bucketHandler.js
@@ -1,4 +1,13 @@
-$.ajax({type: 'POST', url: '/bucketlist/add', success: function(response){
-  console.log(response);
-}});
+function setCookie() {
+  return $.ajax({type: 'POST', url: '/bucketlist/add', success: function(response){
+    console.log(response);
+  }});
+}
+$(document).ready(function(){
+  setCookie().done(function(){
+    $.ajax({type: 'GET', url: '/bucketlist', success: function(response){
+      console.log(response);
+    }});
+  });
+});
 
--- routes/bucket.js
+++ routes/bucket.js
@@ -1,25 +1,22 @@
 function parseCookies (cookie) {
   var list = {},
       rc = cookie;
-
   rc && rc.split(';').forEach(function( cookie ) {
       var parts = cookie.split('=');
       list[parts.shift().trim()] = unescape(parts.join('='));
   });
-
-  console.log(list.session_id);
   return list.session_id;
 }
 
 exports.getBucket = function(db) {
   return function(req, res) {
+    var cookie = parseCookies(req.headers.cookie);
     var collection = db.get('buckets');
-    collection.find({},{},function(events, docs){
-      
-      // var cookie = res.send(req.headers.cookie)
+    cookie = decodeURI(cookie).trim();
+    cookie = cookie.substr(3,(cookie.length - 4));
+    collection.findById(cookie,function(err, docs){
       res.json('buckets', {
-        buckets: docs,
-        cookies: parseCookies(req.headers.cookie)
+        bucket: docs
       });
     });
   }
@@ -27,42 +24,28 @@ exports.getBucket = function(db) {
 
 exports.addBucket = function(db) {
   return function(req, res) {
-
     var collection = db.get('buckets');
     collection.insert({"articles": [{id: 1},{id:2},{id:3}]}, function (err, doc) {
       if (err) {
-          // If it failed, return error
           res.send("There was a problem adding the information to the database.");
       }
       else {
-        // res.end(req.headers.cookie);
         var cookie = parseCookies(req.headers.cookie);
-
         if (cookie === undefined) {
-          //set new cookie
-          // var randomNumber = Math.random().toString();
-          // randomNumber = randomNumber.substring(2, randomNumber.length);
-          res.cookie("session_id", doc._id, {maxAge: 2100000000, httpOnly: true});
-          console.log("doc_id = " + doc._id);
-
-          console.log("cookie created successfully")
-          res.end("test");
+          res.cookie("session_id", doc._id, {maxAge: 2100000000});
+          res.end("cookie created");
         }
         else {
-          //cookie was already present
-          console.log("cookie exists", cookie);
-          // If it worked, set the header so the address bar doesn't still say /adduser
-          // res.location("/");
-          // And forward to success page
-          // res.redirect("/");
-          res.end("didnt work");
+          cookie = decodeURI(cookie).trim();
+          cookie = cookie.substr(3,(cookie.length - 4));
+          res.end("cookie exists = " + cookie);
         }
       }
     });
   }
 }
 
-exports.updateBucket = function(db) {
+exports.updateBucket = function(db, articleId) {
   return function(req, res) {
     var collection = db.get('bucketarticle');
     var collection2 = db.get('articles');
