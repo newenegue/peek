05e85e047f086f02abe8ee26a265ee37456bf382
patriciaho <patricia.p.ho@gmail.com>
2014-03-21 18:26:11 -0700

    Add mongo stuff for heroku

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ var db = monk('localhost:27017/peektest');
 
 var app = express();
 
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+});
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
