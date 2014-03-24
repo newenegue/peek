c04a67720847ea919b4c92b7af1a49f6c640e81d
ElenaHoshizaki <elena.hoshizaki@gmail.com>
2014-03-24 14:09:46 -0700

    trying to troubleshoot intro animation. errors out when eyes are supposed to become happy

--- public/javascripts/introAnimationScript.js
+++ public/javascripts/introAnimationScript.js
@@ -17,6 +17,8 @@ window.onload = function(){
     pointer = document.getElementById("pointer"),
     introBucketStage = document.getElementById("introBucketStage"),
     introBucketEyes = document.getElementById("openEyesIntro"),
+    introLeftEyeFull = document.getElementById("introLeftEyeFull"),
+    introRightEyeFull = document.getElementById("introRightEyeFull"),
     shot2 = document.getElementById("shot2"),
     enterButton = document.getElementsByClassName("closeIntroButton");
 // ------------------------------------------
@@ -51,22 +53,24 @@ window.onload = function(){
 
   //Bucket Appears
   tl.from(introBucketStage, .5, {bottom:-200, autoAlpha:0}, "-=1.5");
+  // tl.from(introBucketEyes, .5, {autoAlpha:0}, "+=.2");
   tl.to(introBucketEyes, .1, {autoAlpha:0}, "+=.2");
-  // tl.to()
+  // tl.to(introRightEyeFull, .1, {css:{display:hidden}}, "+=.2");
 
   //Switch to Second Shot
   tl.from(shot2, .5, {autoAlpha:0}, "+=.5");
-  tl.set(shot2, {top:0, onComplete:introTextContent}, "-=.5");
+  tl.set(shot2, {top:0, onComplete: introTextContent}, "-=.5");
   
   //Switch to Third Shot
   tl.set(shot3, {top:0}, "+=1");
 
   //Enter Button Appears
   tl.from(enterButton, .5, {autoAlpha: 0}, "+=1");
+  tl.restart();
 
   //TO DO: Time bucket so that it becomes happy the exact fraction of a second that the article is dropped into it. Similar to blinking, but only 1x at the precise time instead of every 4 seconds x infinitely
 
-};
+
 
 function introTextContent() {
   var introFront = document.getElementById("introFront");
@@ -76,4 +80,10 @@ function introTextContent() {
   introFront.innerHTML = "Wel";
   introCenter.innerHTML = "c";
   introBack.innerHTML = "ome";
+
+  
+};
+
+
+
 };
